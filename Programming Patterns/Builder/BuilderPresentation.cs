5c78201850b8c7f5a5edcdbe578aa5f4485ff58e
Jaciel Israel Resendiz Ochoa <jresendizochoa@deloitte.com>
2023-03-13 09:31:46 -0600

    Multiple BuildersLoad from Presentation

--- Programming Patterns/Builder/BuilderPresentation.cs
+++ Programming Patterns/Builder/BuilderPresentation.cs
@@ -20,50 +20,60 @@ namespace Programming_Patterns.Builder
         private static void DisplayMenu()
         {
             Console.WriteLine("::: Package Builder :::");
-            if (DecideToStartNewPackage()) 
-            { 
-                CallBuilder();
+            if (DecideToStartNewPackage())
+            {
+                LetUserChooseTypeOfBuilder();
             }
         }
 
         private static bool DecideToStartNewPackage()
         {
             var options = new List<string> { "Exit", "Begin Creation of a Package" };
-            var userInputHandler = new UserInputHandler(
-                options
-            );
+            var userInputHandler = new UserInputHandler(options);
             userInputHandler.Prompt();
 
             bool userWantsToCreateAPackage = userInputHandler.SelectedOption == 1;
             return userWantsToCreateAPackage;
         }
 
-        private static void CallBuilder() 
-        { 
+        private static void LetUserChooseTypeOfBuilder()
+        {
+            var options = new List<string> { "Exit", "Box Package", "Envelope Package" };
+            var userInputHandler = new UserInputHandler(options);
+            userInputHandler.Prompt();
 
+            switch (userInputHandler.SelectedOption)
+            {
+                case 0:
+                    break;
+                case 1:
+                    CallBoxBuilder();
+                    break;
+                case 2:
+                    CallEnvelopeBuilder();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private static void CallEnvelopeBuilder()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void CallBoxBuilder()
+        {
+            Console.WriteLine("::: Box Package Builder :::");
+            Console.WriteLine("Submit the Weight of the Package");
+            float weigh = UserInputHandler.GetFloatFromUser();
+            var boxPackageBuilder = new BoxPackageBuilder(new Contents(weigh));
         }
 
         private static void ShowResultToUser(Package createdPackage)
         {
             Console.WriteLine("Created a New Package:");
             Console.WriteLine(createdPackage);
-        }
-
-        private static string RequestUsername()
-        {
-            string name = String.Empty;
-            while (name.Length == 0)
-            {
-                name = RequireUserName();
-            }
-            return name;
-        }
-
-        private static string RequireUserName()
-        {
-            Console.WriteLine("Username? ");
-            string? rawInput = Console.ReadLine();
-            return rawInput is null ? String.Empty : rawInput.Trim();
         }
 
         public static bool CheckIfContinue()
